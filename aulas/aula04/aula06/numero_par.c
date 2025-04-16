371efa5ec2e67b14922ccd71522f6319a12b74e6
Phoenix8989 <ppassos.augusto@gmail.com>
2025-04-16 10:52:47 -0300

    codigos da aula6

---
+++ aulas/aula04/aula06/numero_par.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+  int numero = 0;
+
+  printf("Entre um numero inteiro: ");
+  scanf("%i", &numero);
+
+  int eh_divisivel_por_2 = numero % 2 == 0;
+   
+  if (eh_divisivel_por_2) {
+    printf("0 numero %i eh par!\n", numero);
+  }
+
+  if (!eh_divisivel_por_2){
+    printf("O numero %i eh impar !\n", numero);
+  } else {
+    printf("O numero%i eh impar!\n", numero);
+  }
+
+  //if(!eh_divisivel_por_2) {
+  //   printf("O numero %i eh impar!\n", numero);
+  // }
+  return 0;
+}
